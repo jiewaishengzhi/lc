782f2275b585fb9044a6e7552ea340d55ec266c9
l224 <1580349337@qq.com>
2026-07-06 00:01:14 +0800

    2026.7.5

--- hot100/linklist/sortList148.java
+++ hot100/linklist/sortList148.java
@@ -1,7 +1,18 @@
 package hot100.linklist;
+/*
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+示例 1：
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
 
-import java.time.chrono.MinguoDate;
+示例 2：
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
 
+示例 3：
+输入：head = []
+输出：[]
+ */
 public class sortList148 {
     public ListNode sortList(ListNode head) {
         if(head==null||head.next==null) return head;
