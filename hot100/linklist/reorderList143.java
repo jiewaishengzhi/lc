59483fd24bc8ff3d70bc0f5c1cf44e36cca7f65f
l224 <1580349337@qq.com>
2026-07-06 14:11:00 +0800

    2026.7.6

--- hot100/linklist/reorderList143.java
+++ hot100/linklist/reorderList143.java
@@ -1,4 +1,8 @@
 package hot100.linklist;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
 L0 → L1 → … → Ln - 1 → Ln
@@ -20,6 +24,78 @@ L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
  */
 public class reorderList143 {
     public void reorderList(ListNode head){
+        //空链表 只有一个节点 只有两个节点 都不需要重排
+        if(head==null||head.next==null||head.next.next==null);
+        //1.找到链表中点
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        ListNode second=slow.next;
+        slow.next=null;
 
+        //2.反转后半部分
+        second=reverse(second);
+
+        //3.交替合并后半部分
+        ListNode first=head;
+
+        while(second!=null){//后半段会比前半段短
+            //保存下一步要走的节点 防止改指针后丢失
+            ListNode temp1=first.next;
+            ListNode temp2=second.next;
+
+            //first后面接second
+            first.next=second;
+            //second后面接first的下一个节点
+            second.next=temp1;
+            //两条链表往后移动
+            first=temp1;
+            second=temp2;
+        }
+
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+
+    public void reorderList2(ListNode head){
+        if(head==null||head.next==null)return;
+
+        //用arraylist装链表
+        List<ListNode> list=new ArrayList<>();
+        ListNode curr=head;
+        while (curr!=null){
+            list.add(curr);
+            curr=curr.next;
+        }
+        int left=0;
+        int right=list.size()-1;
+
+        while (left<right){
+            //前连后
+            list.get(left).next=list.get(right);
+            left++;
+
+            if(left==right)break;
+
+            //后连递增过的前
+            list.get(right).next=list.get(left);
+            right--;
+        }
+        //最后节点置空
+        list.get(left).next=null;
     }
 }
--- hot100/linklist/reverseKGroup25.java
+++ hot100/linklist/reverseKGroup25.java
@@ -1,7 +1,76 @@
 package hot100.linklist;
+/*
+给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 
+示例 1：
+输入：head = [1,2,3,4,5], k = 2
+输出：[2,1,4,3,5]
+
+示例 2：
+输入：head = [1,2,3,4,5], k = 3
+输出：[3,2,1,4,5]
+ */
 public class reverseKGroup25 {
+    /*
+    每组都要做三件事:
+    1.判断后面是否还有k个节点
+    2.翻转这k个节点
+    3.把翻转后的这一组接回原链表
+     */
     public ListNode reverseKGroup(ListNode head, int k){
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
 
+        /*
+        pre：当前要翻转这一组的前一个节点
+        end：当前要翻转这一组的最后一个节点
+        start：当前这一组的第一个节点
+        next：当前这一组后面的第一个节点
+         */
+
+        //pre指向当前要反转的这一组的前一个节点
+        ListNode pre=dummy;
+        ListNode end=dummy;
+
+        while(true){
+            //1.找到当前这一组的k个节点 也就是end 不足k个直接结束
+            for(int i=0;i<k&&end!=null;i++){
+                end=end.next;
+            }
+            //不足k个直接结束
+            if(end==null)break;
+
+            //2.记录这一组的起点和下一组的起点
+            ListNode start=pre.next;
+            ListNode next=end.next;
+
+            //3.先把这一组断开
+            end.next=null;
+
+            //4.翻转这一组
+            pre.next=reverse(start);
+
+            //5.翻转后 连接：start变成这一组的尾节点
+            start.next=next;
+
+            // 6. 移动 pre 和 end，准备处理下一组
+            pre=start;
+            end=pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
     }
 }
