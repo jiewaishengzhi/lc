2c2818412bb2a938f0dfa110360ebbce0093285e
l224 <1580349337@qq.com>
2026-07-13 14:19:55 +0800

    2026.7.13

--- hot100/tree/pathSum437.java
+++ hot100/tree/pathSum437.java
@@ -1,7 +1,65 @@
 package hot100.tree;
 
-public class pathSum437 {
-    public int pathSum(TreeNode root, int targetSum){
+import java.util.HashMap;
+import java.util.Map;
 
+public class pathSum437 {
+    // ------------------- 双递归 -------------------
+    public int pathSum(TreeNode root, int targetSum){
+        if(root==null){
+            return 0;
+        }
+        //以当前节点为起点的路径数+左子树的路径数+右子树的路径数
+        return dfs(root,targetSum)+pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+    }
+
+    //从该起点出发，向下寻找所有可能的路径，计算路径和，统计等于 targetSum 的数量。
+    private int dfs(TreeNode node,long targetSum){
+        if(node==null)return 0;
+        int count=0;
+        if(node.val==targetSum){
+            count++;
+        }
+        //向下延申
+        count+=dfs(node.left,targetSum-node.val);
+        count+=dfs(node.right,targetSum-node.val);
+        return count;
+    }
+
+    //----------------- 前缀和+哈希表-----------------
+    /*
+    每个几点的处理：
+    1.更新currentSum
+    2.查询currentSum-targetSum出现次数
+    3.把currentSum放进map
+    4.递归左右子树
+    5.从map中撤销currentSum
+     */
+    public int pathSum2(TreeNode root,int targetSum){
+        Map<Long,Integer> prefixCount=new HashMap<>();
+        prefixCount.put(0L,1);
+        return dfs(root,0L,targetSum,prefixCount);
+    }
+    private int dfs(TreeNode node,long currentSum,int targetSum,Map<Long,Integer> prefixCount){
+        if(node==null)return 0;
+
+        //1.加上当前节点值 得到根到当前节点的前缀和
+        currentSum+=node.val;
+
+        //2.找到所有以当前节点为结尾的合法路径
+        long need=currentSum-targetSum;
+        int count=prefixCount.getOrDefault(need,0);
+
+        //3.当前前缀和加入哈希表
+        prefixCount.put(currentSum,prefixCount.getOrDefault(currentSum,0)+1);
+
+        //4.分别统计左右子树
+        count+=dfs(node.left,currentSum,targetSum,prefixCount);
+        count+=dfs(node.right,currentSum,targetSum,prefixCount);
+
+        //5.回溯
+        prefixCount.put(currentSum,prefixCount.get(currentSum)-1);
+
+        return count;
     }
 }
--- hot100/哈希表/groupAnagrams49.java
+++ hot100/哈希表/groupAnagrams49.java
@@ -1,4 +1,51 @@
 package hot100.哈希表;
 
+import java.util.*;
+
 public class groupAnagrams49 {
+    //排序作为key
+    public List<List<String>> groupAnagrams(String[] strs){
+        // key：排序后的字符串
+        // value：所有具有相同 key 的原字符串
+        Map<String,List<String>> map=new HashMap<>();
+        for(String str:strs){
+            //字符串转化为字符数组
+            char[] chars=str.toCharArray();
+            //字符数组设为有序
+            Arrays.sort(chars);
+            //构造key
+            String key=new String(chars);
+
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs){
+        Map<String,List<String>> map=new HashMap<>();
+
+        for(String str:strs){
+            int[] count=new int[26];
+
+            for(int i=0;i<str.length();i++){
+                count[str.charAt(i)-'a']++;
+            }
+
+            //构造key
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<26;i++){
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key=sb.toString();
+            List<String> list=map.getOrDefault(key,new ArrayList<>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
